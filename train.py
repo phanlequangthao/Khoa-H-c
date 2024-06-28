698d38c5208f2a7d1c2ae6dd29d87e59a4c6cfce
toolxu1 <chojlaj3@gmail.com>
2024-06-28 11:54:39 +0700

    hello

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=2, restore_best_weig
 history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test), callbacks=[early_stopping])
 
 # Lưu mô hình
-model.save(f"model/model_{num_of_timesteps}.h5")
+model.save(f"model/model_{num_of_timesteps}.keras")
 
 def visualize_loss(history, title):
     loss = history.history["loss"]
